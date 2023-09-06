c5848a0eb31506b1ded8a3b564e19d95a9b6a035
AmitBoricha <borichaamit1@gmail.com>
2023-09-06 22:35:59 +0530

    added

--- Stock-Data-Scraper/stock_scrape.py
+++ Stock-Data-Scraper/stock_scrape.py
@@ -19,3 +19,17 @@ def scrape_stock_data(stock_name: str) -> dict:
 
     url = f"{FINVIZ_BASE_URL}{stock_name}"
     response = requests.get(url, headers=HEADERS)
+
+    if response.status_code == 200:
+        res = {}
+        soup = BeautifulSoup(response.text, 'html.parser')
+
+        stock_symbol = soup.find('h1').find('a', class_='tab-link').text
+        stock_name = soup.select_one('a.tab-link b').get_text(strip=True)
+
+        table = soup.find('table', {'class': 'snapshot-table2'})
+        
+        res['stock_symbol'] = stock_symbol
+        res['stock_name'] = stock_name
+        res['scrape_timestamp'] = datetime.now().strftime("%d-%m-%Y %H:%M:%S")
+        stock_details = {}
