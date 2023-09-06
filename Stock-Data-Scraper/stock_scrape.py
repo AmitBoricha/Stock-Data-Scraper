7dbfd26a0f216ddfce6fa022de557726d49b0915
AmitBoricha <borichaamit1@gmail.com>
2023-09-06 22:35:21 +0530

    added

--- Stock-Data-Scraper/stock_scrape.py
+++ Stock-Data-Scraper/stock_scrape.py
@@ -4,3 +4,18 @@ from datetime import datetime
 
 FINVIZ_BASE_URL = "https://finviz.com/quote.ashx?t="
 HEADERS = {'User-Agent': 'Mozilla'}
+
+
+def scrape_stock_data(stock_name: str) -> dict:
+    """
+    Scrapes stock data from Finviz and returns a dictionary of stock details.
+    
+    Args:
+        stock_name (str): The stock symbol.
+        
+    Returns:
+        dict: A dictionary containing stock details.
+    """
+
+    url = f"{FINVIZ_BASE_URL}{stock_name}"
+    response = requests.get(url, headers=HEADERS)
